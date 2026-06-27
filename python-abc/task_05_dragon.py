7bad66014364e7063fd6588db6503d511881cecf
ghaid999 <gaid.909@gmail.com>
2026-06-27 16:15:13 +0300

    Update task_05_dragon.py

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -5,15 +5,18 @@
 class SwimMixin:
   """ class SwimMixin."""
   def swim(self):
+    """ methode swim"""
     print( "The creature swims!")
 
 class FlyMixin:
   """ class FlyMixin."""
   def fly(self):
+    """ methode fly"""
     prints("The creature flies!")
 
 class Dragon(SwimMixin,FlyMixin):
   """ class Dragon."""
   def rora(self):
+    """ methode rora"""
     prints("The dragon roars!")
   
