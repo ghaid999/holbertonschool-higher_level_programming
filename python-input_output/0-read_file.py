af4a30405d5d1fffde7854d1ab4175be279a4b2a
ghaid999 <gaid.909@gmail.com>
2026-07-02 17:55:29 +0300

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that provides a function to read a UTF-8 text file."""
+
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
