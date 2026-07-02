0e66d7336cc73ff9db517ef02fbd49e828a14417
ghaid999 <gaid.909@gmail.com>
2026-07-02 18:20:01 +0300

    Refine docstrings in save_to_json_file function
    Updated docstrings for clarity and consistency.

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" function that writes an Object to a text file, using a JSON"""
+"""Module that writes an object to a text file using JSON."""
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """write a UTF-8 text file """
+    """Write an object to a text file using a JSON representation."""
     with open(filename, "w", encoding="utf-8") as file:
-         json.dump(my_obj, file)
+        json.dump(my_obj, file)
