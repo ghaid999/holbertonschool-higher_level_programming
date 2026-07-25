243ef845f5faf22316070ec4dae0a50aa36db2dc
ghaid999 <gaid.909@gmail.com>
2026-07-25 21:02:31 +0300

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,5 @@
--- Creates the user user_0d_1 if it does not already exist
+-- Creates user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grants all privileges to user_0d_1
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
