491f66901acbe3c9072ae0dbb8419bb0f210f4c9
ghaid999 <gaid.909@gmail.com>
2026-08-15 21:01:35 +0300

    Update task_02_logic.py

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -4,21 +4,6 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
 @app.route('/items')
 def items():
     with open('items.json', 'r') as file:
